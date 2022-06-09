dec31f1c03ef7795886898a7e90fce8b5a6fae00
Dang Tu <tu07122001@gmail.com>
2022-06-09 18:15:05 +0700

    add

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -20,7 +20,7 @@ async function handler(req, res) {
 
     client.close();
 
-    res.status(201).json({ message: 'Meetup inserted!' });
+    res.status(201).json({ message: 'Meetups inserted!' });
   }
 }
 
